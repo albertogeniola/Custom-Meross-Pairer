27d4d6fc54317097c99088f362673ed216743d20
Alberto Geniola <alberto_geniola@carrefour.com>
2021-03-03 23:28:11 +0100

    Minor UI improvements

--- app/src/main/java/com/albertogeniola/merossconf/ui/fragments/pair/PairDoneFragment.java
+++ app/src/main/java/com/albertogeniola/merossconf/ui/fragments/pair/PairDoneFragment.java
@@ -1,18 +1,32 @@
 package com.albertogeniola.merossconf.ui.fragments.pair;
 
+import android.app.TaskStackBuilder;
+import android.app.job.JobInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.albertogeniola.merossconf.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import cdflynn.android.library.checkview.CheckView;
+
 
 public class PairDoneFragment extends Fragment {
+    private CheckView mCheckView;
+    private Timer mTimer;
+
     public PairDoneFragment() {
-        // Required empty public constructor
+        mTimer = new Timer();
     }
 
     @Override
@@ -25,5 +39,17 @@ public class PairDoneFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pair_done, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mCheckView = view.findViewById(R.id.pairDoneCheckView);
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mCheckView.check();
+            }
+        }, 1500);
     }
 }
